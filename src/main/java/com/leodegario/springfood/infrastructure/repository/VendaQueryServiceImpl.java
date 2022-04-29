d921f01270c423656357c6b15b19bd1f5706bb93
CassioLeodegario <cassio.leodegario@gmail.com>
2022-04-28 21:25:05 -0400

    feat: create custom criteria search

--- src/main/java/com/leodegario/springfood/infrastructure/repository/VendaQueryServiceImpl.java
+++ src/main/java/com/leodegario/springfood/infrastructure/repository/VendaQueryServiceImpl.java
@@ -1,22 +1,42 @@
 package com.leodegario.springfood.infrastructure.repository;
 
 import com.leodegario.springfood.domain.filter.VendaDiariaFilter;
+import com.leodegario.springfood.domain.model.Pedido;
 import com.leodegario.springfood.domain.model.dto.VendaDiaria;
 import com.leodegario.springfood.domain.service.VendaQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
 
-    @Autowired
-    private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager manager;
 
     @Override
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+        var builder = manager.getCriteriaBuilder();
+        var query = builder.createQuery(VendaDiaria.class);
+        var root = query.from(Pedido.class);
 
-        return null;
+        var functionDateDataCriacao = builder.function(
+                "date", Date.class, root.get("dataCriacao"));
+
+        var selection = builder.construct(VendaDiaria.class,
+                functionDateDataCriacao,
+                builder.count(root.get("id")),
+                builder.sum(root.get("valorTotal")));
+
+        query.select(selection);
+        query.groupBy(functionDateDataCriacao);
+
+        return manager.createQuery(query).getResultList();
     }
 
 }

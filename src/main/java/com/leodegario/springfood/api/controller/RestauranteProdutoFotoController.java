28976df0e08d6ab5fd39288f99209b61ef0afc3f
CassioLeodegario <cassio.leodegario@gmail.com>
2022-04-29 20:53:00 -0400

    feat: begining product image upload

---
+++ src/main/java/com/leodegario/springfood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,38 @@
+package com.leodegario.springfood.api.controller;
+
+import com.leodegario.springfood.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(
+            @PathVariable Long restauranteId,
+            @PathVariable Long produtoId,
+            FotoProdutoInput fotoProdutoInput){
+
+        var nomeArquivo = UUID.randomUUID().toString()
+        + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+
+        var arquivoFoto = Path.of("C://test", nomeArquivo);
+        System.out.println(fotoProdutoInput.getDescricao());
+        System.out.println(arquivoFoto);
+        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+
+        try {
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}

9ed9b4bf9033189e5100b56aa993e48ea276a6df
CassioLeodegario <cassio.leodegario@gmail.com>
2022-05-01 16:17:53 -0400

    feat: create custom annotation to validate file content type

--- src/main/java/com/leodegario/springfood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/leodegario/springfood/api/model/input/FotoProdutoInput.java
@@ -1,8 +1,10 @@
 package com.leodegario.springfood.api.model.input;
 
+import com.leodegario.springfood.core.validation.FileContentType;
 import com.leodegario.springfood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -13,6 +15,7 @@ import javax.validation.constraints.NotNull;
 public class FotoProdutoInput {
 
     @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
     @NotNull
     private MultipartFile arquivo;
 
---
+++ src/main/java/com/leodegario/springfood/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,25 @@
+package com.leodegario.springfood.core.validation;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+    private List<String> allowedList;
+
+    @Override
+    public boolean isValid(MultipartFile value, ConstraintValidatorContext constraintValidatorContext) {
+        return value == null || allowedList.contains(value.getContentType());
+    }
+
+
+    //Inicializa a instancia do validador
+    @Override
+    public void initialize(FileContentType constraintAnnotation) {
+        this.allowedList = Arrays.asList(constraintAnnotation.allowed());
+    }
+}

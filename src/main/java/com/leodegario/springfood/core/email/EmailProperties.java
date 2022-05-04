8274f11cbf4b1276ffa33f6fc0e0ee2c82fdfda8
CassioLeodegario <cassio.leodegario@gmail.com>
2022-05-03 20:26:03 -0400

    feat: add fake email service for development environments

--- src/main/java/com/leodegario/springfood/core/email/EmailProperties.java
+++ src/main/java/com/leodegario/springfood/core/email/EmailProperties.java
@@ -5,12 +5,21 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @Component
 @ConfigurationProperties("springfood.email")
 public class EmailProperties {
 
+    private Implementacao impl = Implementacao.FAKE;
+
+    @NotNull
     private String remetente;
+
+    public enum Implementacao {
+        SMTP, FAKE
+    }
 
 }

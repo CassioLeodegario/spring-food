18d8376121346dcfb60d3044764a00b92a1b4ae5
CassioLeodegario <cassio.leodegario@gmail.com>
2022-05-09 07:54:09 -0400

    feat: map problem as error response open api spring fox

--- src/main/java/com/leodegario/springfood/api/exceptionhandler/Problem.java
+++ src/main/java/com/leodegario/springfood/api/exceptionhandler/Problem.java
@@ -1,6 +1,8 @@
 package com.leodegario.springfood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,21 +13,39 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Builder
+@ApiModel("Problema")
 public class Problem {
 
+    @ApiModelProperty(example = "400")
     private Integer status;
+
     private String type;
+
+    @ApiModelProperty(example = "Dados inválidos")
     private String title;
+
+    @ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String detail;
 
+    @ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String userMessage;
+
+    @ApiModelProperty(example = "2022-05-09T07:33:02.70844Z")
     private OffsetDateTime timestamp;
+
+    @ApiModelProperty("Objetos ou campos que geraram o erro")
     private List<Object> objects;
 
+    @ApiModel("ObjetoProblema")
     @Getter
     @Builder
     public static class Object {
+
+
+        @ApiModelProperty(example = "Preço")
         private String name;
+
+        @ApiModelProperty(example = "Preço Obrigatório")
         private String userMessage;
     }
 
--- src/main/java/com/leodegario/springfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/leodegario/springfood/core/openapi/SpringFoxConfig.java
@@ -1,42 +1,54 @@
 package com.leodegario.springfood.core.openapi;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.leodegario.springfood.api.exceptionhandler.Problem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.builders.*;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
 
     @Bean
+    public JacksonModuleRegistrar springFoxJacksonConfig() {
+        return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Bean
     public Docket apiDocket() {
+        TypeResolver typeResolver = new TypeResolver();
+
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.leodegario.springfood.api"))
                 .paths(PathSelectors.any())
-//          .paths(PathSelectors.ant("/restaurantes/*"))
                 .build()
                 .useDefaultResponseMessages(false)
                 .globalResponses(HttpMethod.GET, globalGetResponseMessages())
                 .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+                .additionalModels(typeResolver.resolve(Problem.class))
         .apiInfo(apiInfo())
         .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
@@ -55,6 +67,8 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Erro interno do Servidor")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -68,10 +82,14 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                         .description("Requisição inválida (erro do cliente)")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Erro interno no servidor")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -80,6 +98,8 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
                         .description("Requisição recusada porque o corpo está em um formato não suportado")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(getProblemaModelReference())
                         .build()
         );
     }
@@ -89,12 +109,22 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                         .description("Requisição inválida (erro do cliente)")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Erro interno no servidor")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(getProblemaModelReference())
                         .build()
         );
+    }
+
+    private Consumer<RepresentationBuilder> getProblemaModelReference() {
+        return r -> r.model(m -> m.name("Problema")
+                .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+                        q -> q.name("Problema").namespace("com.leodegario.springfood.api.exceptionhandler")))));
     }
 
 }

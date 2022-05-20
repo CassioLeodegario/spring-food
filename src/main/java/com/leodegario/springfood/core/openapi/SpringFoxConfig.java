bc89cad9a0239a86abcac9e96af23e4d3e8efbbb
CassioLeodegario <cassio.leodegario@gmail.com>
2022-05-19 21:39:39 -0400

    fix: products docs

--- src/main/java/com/leodegario/springfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/leodegario/springfood/core/openapi/SpringFoxConfig.java
@@ -92,6 +92,9 @@ public class SpringFoxConfig {
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(PagedModel.class, PedidoResumoModel.class),
                         PedidosResumoModelOpenApi.class))
+                .alternateTypeRules(AlternateTypeRules.newRule(
+                        typeResolver.resolve(CollectionModel.class, ProdutoModel.class),
+                        ProdutosModelOpenApi.class))
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"),
                         new Tag("Grupos", "Gerencia os grupos de usuários"),

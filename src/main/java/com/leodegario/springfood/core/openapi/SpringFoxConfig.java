5e66f4840004c21b0a0d3beef0cf445ba72d8d4c
CassioLeodegario <cassio.leodegario@gmail.com>
2022-05-19 21:17:48 -0400

    fix: openapi links

--- src/main/java/com/leodegario/springfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/leodegario/springfood/core/openapi/SpringFoxConfig.java
@@ -6,6 +6,7 @@ import com.leodegario.springfood.api.exceptionhandler.Problem;
 import com.leodegario.springfood.api.model.CozinhaModel;
 import com.leodegario.springfood.api.model.PedidoResumoModel;
 import com.leodegario.springfood.api.openapi.model.CozinhasModelOpenApi;
+import com.leodegario.springfood.api.openapi.model.LinksModelOpenApi;
 import com.leodegario.springfood.api.openapi.model.PageableModelOpenApi;
 import com.leodegario.springfood.api.openapi.model.PedidosResumoModelOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Links;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -66,6 +68,7 @@ public class SpringFoxConfig {
                         URL.class, URI.class, URLStreamHandler.class, Resource.class,
                         File.class, InputStream.class)
                 .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+                .directModelSubstitute(Links.class, LinksModelOpenApi.class)
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(Page.class, CozinhaModel.class),
                         CozinhasModelOpenApi.class
